4aacdf8278b2d48c9079d5cd5f9f2f8f084a207c
FernandoUnix <01fernando.ti@gmail.com>
2019-05-04 11:29:54 -0300

    primeiro commit

---
+++ src/main/java/alfa/br/com/gilmario/modelo/mainClass.java
@@ -0,0 +1,9 @@
+package alfa.br.com.gilmario.modelo;
+
+public class mainClass {
+
+    public static void main(String[] args) {
+        var nome = "Fernando";
+        System.out.println(nome);
+    }
+}
